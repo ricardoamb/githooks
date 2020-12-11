5f8925da2b74ce4c77465f0808fb95dbde7646cf
ricardoamb <ricardoamb.dev@gmail.com>
2020-12-11 13:05:02 -0300

    location geolocation

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React, { useState, useEffect } from 'react'
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
-
+  const [ location, setLocation ] = useState({});
+  
   useEffect(() => {
-    async function fetchdata() {
-      const response =  await fetch('https://api.github.com/users/ricardoamb/repos');
-      const data = await response.json();
-      setRepositories(data);
-    }
-    fetchdata();
+    navigator.geolocation.watchPosition(handlePositionReceived);
   }, []);
 
-  useEffect(() => {
-    const filtered = repositories.filter(repo => repo.favorite);
-    document.title = `Ricardo Amb :: ${filtered.length} repositórios favoritos`;
-  }, [repositories]);
-
-  const handleFavorite = id => {
-    const newRepositories = repositories.map(repo => {
-      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
-    })
-    setRepositories(newRepositories);
+  const handlePositionReceived = ({ coords }) => {
+    const { latitude, longitude, accuracy } = coords;
+    setLocation({ latitude, longitude, accuracy })
   }
 
   return (
     <>
-      <ul>
-        { repositories.map(repo => (
-          <li key={repo.id}>
-            {repo.favorite && <span>⭐</span>}{repo.name}
-            <button style={{marginLeft:"10px", border: "none", background: "#6900A8", color: "white"}} onClick={() => handleFavorite(repo.id)}>Favoritar</button>
-          </li> 
-        ))}
-      </ul>
+      Accuracy: {location.accuracy}m<br />
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
     </>
   )
 }
