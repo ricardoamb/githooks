c37264e97840bcb20ec6fdf0bc4add5d1d7db2d2
ricardoamb <ricardoamb.dev@gmail.com>
2020-12-11 13:09:30 -0300

    location componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default function App() {
   const [ location, setLocation ] = useState({});
   
   useEffect(() => {
-    navigator.geolocation.watchPosition(handlePositionReceived);
+    const watchID = navigator.geolocation.watchPosition(handlePositionReceived);
+    return () => navigator.geolocation.clearWatch(watchID);
   }, []);
 
   const handlePositionReceived = ({ coords }) => {
